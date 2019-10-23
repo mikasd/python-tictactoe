2be46d3f7d0460b57551b45607d1807db571b28b
mikasd <michael.ingley@gmail.com>
2019-10-23 14:40:50 -0700

    fixed syntax errors

--- myexample.py
+++ myexample.py
@@ -1,6 +1,6 @@
 ## TIC TAC TOE GAME IN PYTHON
 
-board = ['' for x in range(10)]
+board = [' ' for x in range(10)]
 
 def insertletter(letter, pos):
     board[pos] = letter
@@ -9,16 +9,18 @@ def spaceisfree(pos):
     return board[pos]==' '
 
 def printboard(pos):
-    print('   |   | ')
-    print('' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   | ')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
     print('-----------')
-    print('   |   | ')
-    print('' + board[4] + '|' + board[5] + '|' + board[6])
-    print('   |   | ')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
     print('-----------')
-    print('   |   | ')
-    print('' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+   
 
 
 def iswinner(bo, le):
@@ -38,8 +40,10 @@ def playermove():
                     insertletter('X', move)
                 else: 
                     print('Sorry, this space is already taken, try another space.')
-            else: print('Please type a number between 1 and 9 for your next move.')
-        else: print('Please type a number between 1 and 9')
+            else: 
+                print('Please type a number between 1 and 9 for your next move.')
+        except: 
+            print('Please type a number between 1 and 9')
 
 def compmove():
     possiblemoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
@@ -53,23 +57,48 @@ def compmove():
                 move = i
                 return move
 
-def selectrandom(board):
-    pass
+    cornersopen = []
+    for i in possiblemoves:
+        if i in [1,3,7,9]:
+            cornersopen.append(i)
+            
+    if len(cornersopen) > 0:
+        move = selectrandom(cornersopen)
+        return move
+
+    if 5 in possiblemoves:
+        move = 5
+        return move
+
+    edgesopen = []
+    for i in possiblemoves:
+        if i in [2,4,6,8]:
+            edgesopen.append(i)
+    if len(cornersopen) > 0:
+        move = selectrandom(edgesopen)
+       
+    return move
+
+def selectrandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
 
 def isboardfull(board):
     if board.count(' ') > 1:
-        return True
-    else:
         return False
+    else:
+        return True
 
 def main():
     print('Welcome to Tic Tac Toe!')
-    printboard()
+    printboard(board)
 
     while not(isboardfull(board)):
         if not(iswinner(board, 'O')):
             playermove()
-            printboard()
+            printboard(board)
         else: 
             print('Sorry, O\'s won this time.')
             break
@@ -79,9 +108,9 @@ def main():
             if move == 0:
                 print('Tie Game!')
             else:
-                insertletter('O', board)
+                insertletter('O', move)
                 print('Computer placed an O in position: ', move,)
-                printboard()
+                printboard(board)
         else: 
             print('X\'s won this time! Good job!')
             break
